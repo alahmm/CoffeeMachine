1eebc5fa54c302ffe2d4fb28eb252ff60766ff85
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2022-12-31 10:22:33 +0100

    calculating how much caps of coffee a coffee machine can prepare if there is enough ingredients

--- src/machine/CoffeeMachine.java
+++ src/machine/CoffeeMachine.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     public static int minimal (int a, int b, int c) {
-        return (a < b && a < c) ? a : (b < c && b < a) ? b : c;
+        return (a <= b && a <= c) ? a : (b <= c && b <= a) ? b : c;
     }
     public static void main(String[] args) {
         String output = """
@@ -15,7 +15,6 @@ public class CoffeeMachine {
                 Pouring coffee into the cup
                 Pouring some milk into the cup
                 Coffee is ready!""";
-//        System.out.println("Write how many cups of coffee you will need: ");
         Scanner scanner = new Scanner(System.in);
 /*        int numberOfCups = scanner.nextInt();
         System.out.printf("For %d cups of coffee you will need:", numberOfCups);
@@ -30,28 +29,17 @@ public class CoffeeMachine {
         int gramsOfCoffeeBeans = scanner.nextInt();
         System.out.println("Write how many cups of coffee you will need: ");
         int numberOfCups = scanner.nextInt();
-        int sum = (numberOfCups * 200) +
-                (numberOfCups * 50) + (numberOfCups * 15);
-        if ((mlOfWater + mlOfMilk + gramsOfCoffeeBeans ) == sum) {
+        long mlOfwaterRound = Math.round((double)mlOfWater / (numberOfCups * 200));
+        long mlOfMilkRound = Math.round((double)mlOfMilk / (numberOfCups * 50));
+        long gramesOfCoffeeRound = Math.round((double)gramsOfCoffeeBeans / (numberOfCups * 15));
+        if ((numberOfCups == 0 && (mlOfWater == 0 || mlOfMilk == 0 || gramsOfCoffeeBeans == 0)) ||
+                minimal(mlOfWater / 200, mlOfMilk / 50, gramsOfCoffeeBeans) - numberOfCups == 0) {
             System.out.println("Yes, I can make that amount of coffee");
-        } else if ((Math.round((double)mlOfWater / (numberOfCups * 200)) == 1 &&
-                Math.round((double)mlOfMilk / (numberOfCups * 50)) > 1 &&
-                Math.round((double)gramsOfCoffeeBeans / (numberOfCups * 15)) > 1)||
-                (Math.round((double)mlOfWater / (numberOfCups * 200)) > 1 &&
-                        Math.round((double)mlOfMilk / (numberOfCups * 50)) == 1 &&
-                        Math.round((double)gramsOfCoffeeBeans / (numberOfCups * 15)) > 1) ||
-                (Math.round((double)mlOfWater / (numberOfCups * 200)) > 1 &&
-                        Math.round((double)mlOfMilk / (numberOfCups * 50)) > 1 &&
-                        Math.round((double)gramsOfCoffeeBeans / (numberOfCups * 15)) == 1)) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else if ((mlOfWater + mlOfMilk + gramsOfCoffeeBeans ) > sum || (Math.round((double)mlOfWater / (numberOfCups * 200)) > 1 &&
-                Math.round((double)mlOfMilk / (numberOfCups * 50)) > 1 &&
-                Math.round((double)gramsOfCoffeeBeans / (numberOfCups * 15)) > 1)) {
-            int numberOfAdditionalCups = (mlOfWater + mlOfMilk + gramsOfCoffeeBeans ) / sum;
+        } else if ((numberOfCups == 0 && mlOfWater != 0 && mlOfMilk != 0 && gramsOfCoffeeBeans != 0)||
+                minimal((int)mlOfwaterRound , (int)mlOfMilkRound, (int)gramesOfCoffeeRound) > 1) {
+            int numberOfAdditionalCups = minimal(mlOfWater / 200 , mlOfMilk / 50 , gramsOfCoffeeBeans / 15) - numberOfCups;
             System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", numberOfAdditionalCups);
-        } else if (numberOfCups !=0 && (Math.round((double)mlOfWater / (numberOfCups * 200)) < 1 ||
-                Math.round((double)mlOfMilk / (numberOfCups * 50)) < 1 ||
-                Math.round((double)gramsOfCoffeeBeans / (numberOfCups * 15)) < 1)) {
+        } else {
                 int numberOfPossibleCups = minimal(mlOfWater / 200 , mlOfMilk / 50 , gramsOfCoffeeBeans / 15);
             System.out.printf("No, I can make only %d cup(s) of coffee", numberOfPossibleCups);
 
